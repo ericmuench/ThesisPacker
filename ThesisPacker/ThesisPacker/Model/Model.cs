3d3a204e486c83b8b187931cdf9125450278c694
Eric Münch <eric-muench@outlook.de>
2021-12-29 14:27:33 +0100

    Created Model

---
+++ ThesisPacker/ThesisPacker/Model/Model.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace ThesisPacker.Model
+{
+    public class ThesisPackerConfig
+    {
+        #region Fields
+        public List<string> Files { get; }
+        public List<GitProject> GitProjects { get; }
+        #endregion
+
+        #region Constructors
+        public ThesisPackerConfig(List<string> files, List<GitProject> gitProjects)
+        {
+            Files = files;
+            GitProjects = gitProjects;
+        }
+        #endregion
+    }
+
+    public class GitProject
+    {
+        #region Fields
+        public string Name { get; }
+        public string Url { get; }
+        public List<string> IgnoredBranches { get; }
+        #endregion
+
+        #region Constructors
+        public GitProject(string name, string url, List<string> ignoredBranches)
+        {
+            Name = name;
+            Url = url;
+            IgnoredBranches = ignoredBranches;
+        }
+
+        public GitProject(string name, string url) : this(name, url, new List<string>()) { }
+        #endregion
+    }
+}
---
+++ ThesisPacker/ThesisPacker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThesisPacker.Model;
+
+namespace ThesisPacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testProj = new GitProject("Test", "https://gitlab.hof-university.de/Eric/sta_csharp_ws2020_21/-/tree/master/DJSets/DJSets");
+            var testConfig = new ThesisPackerConfig(
+                new List<string> { @"C:\Users\Eric\Documents\Hochschule Hof\WS 2021_22 (7)\projektarbeit_ws21_22\Praxisarbeit.pdf" }, 
+                new List<GitProject> { testProj }
+            );
+
+            Console.WriteLine($"TestConfig: Files:{String.Join(",",testConfig.Files)}, Projects: {String.Join(",", testConfig.GitProjects.Select(it => it.Name))}");
+            Console.ReadLine();
+        }
+    }
+}
